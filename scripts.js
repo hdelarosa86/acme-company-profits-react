5ae2cf8d597eec175f865076ea1aeef0714ff1f1
Hector DelaRosa <hdelaros@gmail.com>
2020-08-21 15:46:17 -0400

    Rendered nested routes and update components

--- scripts.js
+++ scripts.js
@@ -1,70 +1,162 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable react/react-in-jsx-scope */
 const { render } = ReactDOM;
 const { Component } = React;
 const { HashRouter, Switch, Link, Route, Redirect } = ReactRouterDOM;
 const app = document.querySelector('#app');
 
-const Nav = props => {
-  const path = props.location.pathname;
+const Nav = ({ pathname, companies }) => {
+  const path = pathname;
   return (
     <nav>
-      <Link to="/companies">Companies</Link>
-      <Link to="/products">Products</Link>
+      <Link to="/" className={path === '/' ? 'selected' : ''}>
+        Acme Company Profits with React Router
+      </Link>
+      <Link
+        to="/companies"
+        className={path.startsWith('/companies') ? 'selected' : ''}
+      >
+        {`Companies (${companies.length})`}
+      </Link>
     </nav>
   );
 };
 
-const Navigation = () => {
-  return (
-    <HashRouter>
-      <Route component={Nav} />
-      <Switch>
-        <Route path="/companies" component={Companies} />
-        <Route path="/products" component={Products} />
-        <Redirect to="/companies" />
-      </Switch>
-    </HashRouter>
-  );
-};
+const Home = () => (
+  <div className="home">
+    <h1>Welcome!</h1>
+  </div>
+);
 
-
-const CompaniesNames = props => {
-  if (!props) {
-    return (<div>Loading...</div>)
+const CompaniesList = ({ companies, match }) => {
+  if (!companies) {
+    return <div>Loading...</div>;
   }
+  const path = match.params.id;
   return (
-    <ul>
-      {props.companies.map(company => {
-        return <li><Link to={`/companies/${company.id}`}>{company.name}</Link></li>
-      })}
-    </ul>
+    <div className="company-card">
+      <ul className="companies">
+        {companies.map((company, idx) => {
+          return (
+            <li>
+              <Link
+                key={idx}
+                to={`/companies/${company.id}`}
+                className={path === company.id ? 'selected' : ''}
+              >
+                {company.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Route
+        path="/companies/:id"
+        render={(props) => <CompanyProfitList {...props} />}
+      />
+    </div>
   );
 };
+
+class CompanyProfitList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profits: [],
+      isError: false,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      const { id } = this.props.match.params;
+      axios
+        .get(
+          `https://acme-users-api-rev.herokuapp.com/api/companies/${id}/companyProfits`
+        )
+
+        .then(({ data }) => {
+          const profits = data;
+          this.setState({ profits, isError: false });
+        })
+        .catch((e) => {
+          this.setState({ profits: [], isError: true });
+        });
+    }
+  }
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    axios
+      .get(
+        `https://acme-users-api-rev.herokuapp.com/api/companies/${id}/companyProfits`
+      )
+      .then((response) => {
+        const profits = response.data;
+        this.setState({ profits });
+      })
+      .catch((e) => {
+        this.setState({ profits: [] });
+      });
+  }
+
+  render() {
+    const { profits, isError } = this.state;
+    if (isError) return <h3>Company not found</h3>;
+    return (
+      <ul className="profits">
+        {profits.map((profit) => (
+          <li>
+            <div>
+              <p>
+                <span className="fiscal-year">
+                  {profit.fiscalYear.slice(0, 4)}
+                </span>{' '}
+                <span>${profit.amount.toFixed(2)}</span>
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       companies: [],
-      products : [], 
-      offerings: [],
     };
   }
   componentDidMount() {
-    Promise.all([
-      axios.get('https://acme-users-api-rev.herokuapp.com/api/companies'),
-      axios.get('https://acme-users-api-rev.herokuapp.com/api/products'),
-      axios.get('https://acme-users-api-rev.herokuapp.com/api/offerings'),
-    ])
-      .then(responses => responses.map(response => response.data))
-      .then( ([companies, products, offerings ])=> this.setState({ companies, products, offerings}));
-     
+    axios
+      .get('https://acme-users-api-rev.herokuapp.com/api/companies')
+      .then((response) => {
+        const companies = response.data;
+        this.setState({ companies });
+      });
   }
   render() {
+    const { companies } = this.state;
     return (
       <HashRouter>
-        <Route component={Nav}/>
-        <Route path="/companies" render={ () => <CompaniesNames companies={[this.state.companies, [1,2,3]]} /> }/>
-        <Route path="/companies/:id" />
+        <Route
+          render={({ location }) => (
+            <Nav pathname={location.pathname} companies={companies} />
+          )}
+        />
+        <Switch>
+          <Route
+            path="/companies/:id?"
+            render={({ match }) => (
+              <CompaniesList match={match} companies={companies} />
+            )}
+          />
+          <Route path="/" render={() => <Home />} />
+          <Redirect to="/" />
+        </Switch>
       </HashRouter>
     );
   }
